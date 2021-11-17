bd5e8e52b13e79d5066b6c577d75f54502eff530
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2021-11-17 14:59:13 +0100

    my playgrund try to break react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './Hello';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello greering={'Hello React Ninja'} />
   </React.StrictMode>,
   document.getElementById('root')
 );
